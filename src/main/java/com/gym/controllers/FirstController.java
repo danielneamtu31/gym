57c0985d072e8dc48e8ff83c83e8f06fe0071648
Dani <neamtudaniel31@yahoo.ro>
2017-11-05 14:19:23 +0200

    database config

--- src/main/java/com/gym/controllers/FirstController.java
+++ src/main/java/com/gym/controllers/FirstController.java
@@ -1,14 +1,23 @@
 package com.gym.controllers;
 
+import com.gym.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/login")
 public class FirstController {
 
+
+
     @RequestMapping(value = "/loginUser")
     public String loginUser() {
         return "welcome";
     }
+
+
 }
--- src/main/java/com/gym/model/User.java
+++ src/main/java/com/gym/model/User.java
@@ -1,4 +1,43 @@
 package com.gym.model;
 
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table( name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    public User(){
+        this.firstName = "";
+    }
+
+    public User(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
