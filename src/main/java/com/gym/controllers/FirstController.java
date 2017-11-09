6f45bbddb54cec51a0d8547e0771be0371892cf3
Dani <neamtudaniel31@yahoo.ro>
2017-11-10 00:09:11 +0200

    database config

--- src/main/java/com/gym/controllers/FirstController.java
+++ src/main/java/com/gym/controllers/FirstController.java
@@ -4,6 +4,7 @@ import com.gym.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -13,10 +14,16 @@ import javax.annotation.Resource;
 public class FirstController {
 
 
+    private UserRepository userRepository;
+
 
     @RequestMapping(value = "/loginUser")
-    public String loginUser() {
-        return "welcome";
+    public String loginUser(Model model)
+    {
+        userRepository = new UserRepository();
+        userRepository.bla();
+        model.addAttribute("user", "dan");
+       return "welcome";
     }
 
 
